4d317ac3761e161e183281c80087d323ae1e4547
James Anderson <james.anderson98@outlook.com>
2024-05-02 12:41:42 +0100

    Loop through each image and create HTML element, append to frame

--- src/index.js
+++ src/index.js
@@ -17,3 +17,31 @@ navDotsContainer.classList.add('dots');
 // define image height and width
 const imgWidth = '600';
 const imgHeight = imgWidth;
+
+
+// loop through each image, set dimension and append to carousel element
+imgArr.forEach((image, index) => {
+    console.log(image);
+    const img = new Image();
+    img.src = image;
+    img.height = imgHeight;
+    img.width = imgWidth;
+    img.classList.add('image');
+    img.setAttribute("id", index);
+
+
+    const navDot = document.createElement('div');
+    navDot.classList.add('nav-dot');
+    navDot.setAttribute("id", `dot-${index}`)
+    navDot.setAttribute("data-id", index);
+    // set first image as active
+    if (index === 0) {
+        img.classList.add('active');
+        navDot.classList.add('active')
+    }
+
+    // append
+    frame.appendChild(img);
+    navDotsContainer.appendChild(navDot);
+    frame.appendChild(navDotsContainer);
+})
