e4ee0906d15df6edd5126b7ee8851f4bde4ba38d
James Anderson <james.anderson98@outlook.com>
2024-05-02 12:40:58 +0100

    Add function to bulk import slideshow images

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import './style.css';
 import rArrow from './images/right-arrow.svg';
 import lArrow from './images/left-arrow.svg';
+
+// import all images in one
+function importAll(r) {
+    return r.keys().map(r);
+  }
+  
+const imgArr = importAll(require.context('./images', false, /\.(png|jpe?g)$/));
+
+
 const frame = document.querySelector('.frame');
 const navDotsContainer = document.createElement('div');
 navDotsContainer.classList.add('dots');
