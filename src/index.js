e1d68188f9e1a31b9e9766c38aa55691db1a5f76
James Anderson <james.anderson98@outlook.com>
2024-05-02 12:40:22 +0100

    Import stylesheet and nav arrows

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import './style.css';
+import rArrow from './images/right-arrow.svg';
+import lArrow from './images/left-arrow.svg';
 const frame = document.querySelector('.frame');
 const navDotsContainer = document.createElement('div');
 navDotsContainer.classList.add('dots');
