15a1923ec295dc6cad07a0867d2256255ffcadb1
James Anderson <james.anderson98@outlook.com>
2024-05-02 12:32:47 +0100

    Select frame element and create container for navDots. Set image dimensions

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const frame = document.querySelector('.frame');
+const navDotsContainer = document.createElement('div');
+navDotsContainer.classList.add('dots');
+
+// define image height and width
+const imgWidth = '600';
+const imgHeight = imgWidth;
